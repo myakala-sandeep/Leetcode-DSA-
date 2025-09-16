66ac5fc5c23fe369a2261787f3e998ffc4f85f91
myakala-sandeep <mekalasandeep74@gmail.com>
2025-09-16 09:34:48 +0530

    485.max-consecutive-ones.java added

--- 189.rotate-array.java
+++ 189.rotate-array.java
@@ -1,18 +1,12 @@
-/*
- * @lc app=leetcode id=189 lang=java
- *
- * [189] Rotate Array
- */
 
-// @lc code=start
 class Solution {
     public void rotate(int[] nums, int k) {
             int n = nums.length;
     k = k % n;
 
-    reverse(nums, 0, n - 1);     // reverse whole array
-    reverse(nums, 0, k - 1);     // reverse first k elements
-    reverse(nums, k, n - 1);     // reverse remaining elements
+    reverse(nums, 0, n - 1);     
+    reverse(nums, 0, k - 1);    
+    reverse(nums, k, n - 1);     
 }
 
 private void reverse(int[] nums, int start, int end) {
@@ -26,5 +20,5 @@ private void reverse(int[] nums, int start, int end) {
 }
         
 }
-// @lc code=end
+
 
---
+++ 485.max-consecutive-ones.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=485 lang=java
+ *
+ * [485] Max Consecutive Ones
+ */
+
+// @lc code=start
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
