367c68ed566924801de10a56503c44e04030b250
myakala-sandeep <mekalasandeep74@gmail.com>
2025-09-18 06:18:21 +0530

    35.search-insert-position.java added

---
+++ Arrays/35.search-insert-position.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+    }
+}
+// @lc code=end
+
