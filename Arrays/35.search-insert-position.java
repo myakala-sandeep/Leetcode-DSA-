cabd8676f30366c63cb85266e2ef3f41f2be8459
myakala-sandeep <mekalasandeep74@gmail.com>
2025-09-18 07:42:51 +0530

    update 35

--- Arrays/35.search-insert-position.java
+++ Arrays/35.search-insert-position.java
@@ -6,8 +6,31 @@
 
 // @lc code=start
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        
+    public int searchInsert(int[] nums, int  k) {
+          int start =0;
+  int end=nums.length-1;
+  
+  int index=-1;
+  while(start<=end)
+  {
+     int mid=start+(end-start)/2;
+    if(nums[mid]<k)
+    {
+        start=mid+1;
+        index=mid;
+    }
+    else if(nums[mid]>k)
+    {
+        end=mid-1;
+        //  index=mid;
+    }
+    else if(nums[mid]==k)
+    {
+        return mid;
+    }
+  }
+     return index+1;  
+      
     }
 }
 // @lc code=end
