79e1857365740f506c11a091d543917b68f8a682
myakala-sandeep <mekalasandeep74@gmail.com>
2025-09-20 22:57:16 +0530

    704.binary-search.java added

--- Arrays/42.trapping-rain-water.java
+++ Arrays/42.trapping-rain-water.java
@@ -1,3 +1,4 @@
+
 /*
  * @lc app=leetcode id=42 lang=java
  *
@@ -7,6 +8,42 @@
 // @lc code=start
 class Solution {
     public int trap(int[] height) {
+         int trap=0;
+      int left =0;
+      int  right=height.length-1;
+      int leftmax=0;
+      int rightmax=0;
+      while(left<right)
+      {
+        if(height[left]<height[right])
+        {
+            if(leftmax<=height[left])
+            {
+                leftmax=height[left];
+            }
+            else
+            {
+                trap=trap+(leftmax-height[left]);
+            }
+            left++;
+
+        }
+        else
+        {
+            if(rightmax<=height[right])
+            {
+             rightmax=height[right];
+            }
+            else
+            {
+                 trap=trap+(rightmax-height[right]);
+            }
+            right--;
+        }
+         
+      }
+      return trap;
+        
         
     }
 }
