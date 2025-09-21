2dd645eaaed62a342a6391388de7a58f1439471c
myakala-sandeep <mekalasandeep74@gmail.com>
2025-09-21 21:12:02 +0530

    updated

--- Arrays/even-number-bitwiseORs.java
+++ Arrays/even-number-bitwiseORs.java
@@ -2,27 +2,27 @@
     public int evenNumberBitwiseORs(int[] nums) {
         int left =0;
         int right=nums.length-1;
-        int bit =0;
+        int bitvalue =0;
         while(left<=right)
         {
                 if(left==right)
                 {
                      if(nums[left]%2==0)
-                     bit=bit|nums[left];
+                     bitvalue=bitvalue|nums[left];
                     break;
                     
                 }
                 if(nums[left]%2==0)
                 {
-                    bit=bit|nums[left];
+                    bitvalue=bitvalue|nums[left];
                 }
                    if(nums[right]%2==0)
                   {
-                    bit=bit|nums[right];
+                    bitvalue=bitvalue|nums[right];
                   }
             left++;
             right--;
         }
-      return bit;  
+      return bitvalue;  
     }
 }
