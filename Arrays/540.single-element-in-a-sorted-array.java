98d2108fa30e65a7a49c872eea5f78da8c3f14e2
myakala-sandeep <mekalasandeep74@gmail.com>
2025-09-22 20:28:33 +0530

    34.find-first-and-last-position-of-element-in-sorted-array.java added

--- Arrays/540.single-element-in-a-sorted-array.java
+++ Arrays/540.single-element-in-a-sorted-array.java
@@ -8,6 +8,35 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         
+        int left=0;
+        int right=nums.length-1;
+        while(left<right)
+        {
+             int mid=left+(right-left)/2;
+           if(mid%2==0)
+           {
+            if(nums[mid]!=nums[mid+1])
+            {
+                right=mid;
+            }
+            else
+            left=mid+1;
+           }
+           else
+           {
+             if(nums[mid]!=nums[mid-1])
+            {
+                right=mid;
+            }
+            else
+            left=mid+1;
+
+
+           }
+
+        }
+
+        return nums[left];
     }
 }
 // @lc code=end
