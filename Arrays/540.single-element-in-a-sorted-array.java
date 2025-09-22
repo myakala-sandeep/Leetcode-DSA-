3e9469f53516c84c576d42a7be13b5ea7f17cc58
myakala-sandeep <mekalasandeep74@gmail.com>
2025-09-22 09:24:20 +0530

    540.single-element-in-a-sorted-array.java added

---
+++ Arrays/540.single-element-in-a-sorted-array.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=540 lang=java
+ *
+ * [540] Single Element in a Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
