5f3fef3c25e36002ae53716a8596d9ae4ee3f0a0
myakala-sandeep <mekalasandeep74@gmail.com>
2025-09-23 23:28:46 +0530

    162.find-peak-element.java

---
+++ Arrays/162.find-peak-element.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=162 lang=java
+ *
+ * [162] Find Peak Element
+ */
+
+// @lc code=start
+class Solution {
+    public int findPeakElement(int[] nums) {
+         int left =0;
+        int right=nums.length-1;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]<nums[mid+1])
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid;
+            }
+        }
+       return left; 
+
+        
+    }
+}
+// @lc code=end
+
