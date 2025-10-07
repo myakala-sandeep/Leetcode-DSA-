aefb71f1573e80a08010d0b8279357c66c1a75c3
myakala-sandeep <mekalasandeep74@gmail.com>
2025-10-07 23:16:25 +0530

    1759.count-number-of-homogenous-substrings.java

---
+++ Strings/1759.count-number-of-homogenous-substrings.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1759 lang=java
+ *
+ * [1759] Count Number of Homogenous Substrings
+ */
+
+// @lc code=start
+class Solution {
+    public int countHomogenous(String s) {
+        
+    }
+}
+// @lc code=end
+
