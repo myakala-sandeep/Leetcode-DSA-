bf05e4391999a9e42155a283736ff0d821f8ccb8
myakala-sandeep <mekalasandeep74@gmail.com>
2025-10-13 23:26:55 +0530

    561.array-partition.java

---
+++ Arrays/561.array-partition.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=561 lang=java
+ *
+ * [561] Array Partition
+ */
+
+// @lc code=start
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
