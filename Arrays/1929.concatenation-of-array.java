c732704a30d901bacf56a9241703b62edd15d865
myakala-sandeep <mekalasandeep74@gmail.com>
2025-10-14 23:31:55 +0530

    1929.concatenation-of-array.java

---
+++ Arrays/1929.concatenation-of-array.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1929 lang=java
+ *
+ * [1929] Concatenation of Array
+ */
+
+// @lc code=start
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+
+
+           int n = nums.length;
+        int[] ans = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];         // First half
+            ans[i + n] = nums[i];     // Second half (duplicate)
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
