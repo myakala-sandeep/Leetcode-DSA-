c9a1ae9848989b4b14d7add96a35f96238c65d38
myakala-sandeep <mekalasandeep74@gmail.com>
2025-10-21 22:43:56 +0530

    1004.max-consecutive-ones-iii.java

---
+++ Arrays/1004.max-consecutive-ones-iii.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1004 lang=java
+ *
+ * [1004] Max Consecutive Ones III
+ */
+
+// @lc code=start
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+
+        int left = 0, maxLen = 0, zeroCount = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+            
+            while (zeroCount > k) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        
+        return maxLen;
+        
+    }
+}
+// @lc code=end
+
